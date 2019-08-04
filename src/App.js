723116bd644b85da9b378ec5bc6b58c1114cef04
Vika <victoriaspiridonova95@gmail.com>
2019-08-05 00:42:02 +0700

    -header -navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.scss';
 import {Navbar} from "./components/navbar/navbar.component";
 import {Header} from "./components/header/header.component";
+import {Carousel} from "./components/carousel-banner/carousel.banner.component";
 
 export class App extends React.Component {
     render() {
@@ -9,6 +10,7 @@ export class App extends React.Component {
             <div className="App">
                 <Header/>
                 <Navbar/>
+                <Carousel/>
             </div>
         );
     }
--- src/components/carousel-banner/carousel.banner.component.js
+++ src/components/carousel-banner/carousel.banner.component.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import imgShopOne from '../../img/shop.jpg';
+import imgShopTwo from '../../img/shooping-img-two.jpg';
+import "../carousel-banner/carousel.banner.component.scss";
+
 
 export class Carousel extends React.Component {
     render() {
         return (
-            <div></div>
+            <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src={imgShopOne} className="d-block w-100" alt="img"/>
+                    </div>
+                    <div className="carousel-item">
+                        <img src={imgShopTwo} className="d-block w-100" alt="img"/>
+                    </div>
+                    <div className="carousel-item">
+                        <img src={imgShopOne} className="d-block w-100" alt="img"/>
+                    </div>
+                </div>
+                <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a>
+            </div>
         )
     }
 }
