4e4d9b7b93e600c6618f9a74aeccb5cd2cea3d41
Vika <victoriaspiridonova95@gmail.com>
2019-08-14 19:27:42 +0700

    -top (array)

---
+++ src/data/products.js
@@ -0,0 +1,58 @@
+export const topProducts = [
+    {
+        imgBefore: 'top-01-1.jpg',
+        imgAfter: 'top-01-2.jpg',
+        category: 'Children',
+        name: 'Children\'s Winter Suit',
+        price: '$100',
+    },
+    {
+        imgBefore: 'top-02-1.jpg',
+        imgAfter: 'top-02-2.jpg',
+        category: 'Women',
+        name: 'Women\'s Dress',
+        price: '$100',
+    },
+    {
+        imgBefore: 'top-03-1.jpg',
+        imgAfter: 'top-03-2.jpg',
+        category: 'Men',
+        name: 'Men\'s Shirt',
+        price: '$100',
+    },
+    {
+        imgBefore: 'top-04-1.jpg',
+        imgAfter: 'top-04-2.jpg',
+        category: 'Women',
+        name: 'Women\'s Dress',
+        price: '$100',
+    },
+    {
+        imgBefore: 'top-05-1.jpg',
+        imgAfter: 'top-05-2.jpg',
+        category: 'Women',
+        name: 'Women\'s Sweatshirt',
+        price: '$100',
+    },
+    {
+        imgBefore: 'top-06-1.jpg',
+        imgAfter: 'top-06-2.jpg',
+        category: 'Women',
+        name: 'Women\'s Dress',
+        price: '$100',
+    },
+    {
+        imgBefore: 'top-07-1.jpg',
+        imgAfter: 'top-07-2.jpg',
+        category: 'Children',
+        name: 'Baby Doll',
+        price: '$100',
+    },
+    {
+        imgBefore: 'top-08-1.jpg',
+        imgAfter: 'top-08-2.jpg',
+        category: 'Women',
+        name: 'Women\'s T-shirt',
+        price: '$100',
+    }
+];
