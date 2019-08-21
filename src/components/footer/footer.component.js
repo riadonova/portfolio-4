37fa880de89ed2a7c3db07d3c83cf0dccf86a77a
Vika <victoriaspiridonova95@gmail.com>
2019-08-21 22:23:43 +0700

    -footer -tape -top

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -12,13 +12,13 @@ export class Footer extends React.Component {
         return (
             <div className="container footer">
                 <div className="row mt-5 mb-3">
-                    <div className="col-sm-3">
+                    <div className="col-lg-3">
                         <h1 className="mb-3">STAR<span className="text-dark">SHOP</span></h1>
                         <p className="text-secondary">StarShop is an enchanting and easy to use theme that allows you to
                             sell your products in a dynamic way.</p>
                         <p className="text-secondary">This theme is a partial copy of the original performance.</p>
                     </div>
-                    <div className="col-sm-3">
+                    <div className="col-lg-3">
                         <h5 className="font-weight-bold text-dark mb-3">Useful Links</h5>
                         <ul className="text-secondary p-0">
                             <li><a href="#"><i className="material-icons mr-2">
@@ -41,7 +41,7 @@ export class Footer extends React.Component {
                                 Terms and Conditions</a></li>
                         </ul>
                     </div>
-                    <div className="col-sm-3">
+                    <div className="col-lg-3">
                         <h5 className="font-weight-bold text-dark mb-3">Information</h5>
                         <ul className="text-secondary p-0">
                             <li><a href="#"><i className="material-icons mr-2">
@@ -64,7 +64,7 @@ export class Footer extends React.Component {
                                 Modern Process</a></li>
                         </ul>
                     </div>
-                    <div className="col-sm-3">
+                    <div className="col-lg-3">
                         <h5 className="font-weight-bold text-dark mb-3">Contact Info</h5>
                         <ul className="text-secondary p-0">
                             <li><a href="#"><i className="material-icons mr-2">
@@ -78,28 +78,30 @@ export class Footer extends React.Component {
                                 123-456-789</a></li>
                         </ul>
                         <h5 className="font-weight-bold text-dark mb-3">Newsletter</h5>
-                        <input className="form-control mr-sm-2" type="email" placeholder="Enter your email"/>
-                        <button className="btn btn-outline-primary my-2 my-sm-0 bg-primary" type="submit"><i
+                        <div className="input-group">
+                        <input className="form-control" type="email" placeholder="Enter your email"/>
+                        <button className="btn btn-outline-primary my-2 my-sm-0 bg-primary input-group-append" type="submit"><i
                             className="material-icons text-white i-send">
                             send</i></button>
+                        </div>
                     </div>
                 </div>
                 <div className="footer-bottom mb-3">
                     <div className="row pt-4 pb-3">
-                        <div className="col-sm-6 text-secondary">
+                        <div className="col-lg-6 text-secondary">
                             StarShop Mobile app is Available now. Download it now on your favorite device and indulge
                             in a seamless shopping experience.
                         </div>
-                        <div className="col-sm-6 store-btn text-secondary">
+                        <div className="col-lg-6 store-btn text-secondary">
                             <a href="#"><img src={imgGooPl} alt="img"/></a>
                             <a href="#"><img className="mr-2" src={imgAppStr} alt="img"/></a>
                         </div>
                     </div>
                     <div className="row mt-3">
-                        <div className="col-sm-6 text-secondary">
+                        <div className="col-lg-6 text-secondary">
                             <p>© StarShop 2019</p>
                         </div>
-                        <div className="col-sm-6 img-card">
+                        <div className="col-lg-6 img-card">
                             <img src={imgCardOne} alt="img"/>
                             <img className="mr-2" src={imgCardTwo} alt="img"/>
                             <img className="mr-2" src={imgCardThree} alt="img"/>
--- src/components/tape-insta/tape.insta.component.js
+++ src/components/tape-insta/tape.insta.component.js
@@ -10,26 +10,42 @@ import imgInstSix from '../../img/insta-06.jpg';
 export class Tape extends React.Component {
     render() {
         return (
-            <div className="text-center pb-5 tape-inst">
-                <h2 className="font-weight-bold pt-1 pb-1">@starshop</h2>
+            <div className="text-center pb-5 tape-inst overflow-hidden">
+                <h2 className="font-weight-bold py-2">@starshop</h2>
                 <p className="text-secondary mb-4">Follow us on Instagram</p>
                 <div className="row">
-                    <div className="col-sm-2 img-inst">
+                    <div className="col-md-2 img-inst">
                         <img src={imgInstOne} alt="img"/>
+                        <div className="inst-after text-white">
+                            <div className="inst-meta">
+                            <div className="inst-item mx-1">
+                            <i className="material-icons mx-1">
+                                favorite
+                            </i>
+                            <span>8</span>
+                            </div>
+                            <div className="inst-item mx-1">
+                            <i className="material-icons mx-1">
+                                chat_bubble
+                            </i>
+                            <span>0</span>
+                            </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-sm-2 img-inst">
+                    <div className="col-md-2 img-inst">
                         <img src={imgInstTwo} alt="img"/>
                     </div>
-                    <div className="col-sm-2 img-inst">
+                    <div className="col-md-2 img-inst">
                         <img src={imgInstThree} alt="img"/>
                     </div>
-                    <div className="col-sm-2 img-inst">
+                    <div className="col-md-2 img-inst">
                         <img src={imgInstFour} alt="img"/>
                     </div>
-                    <div className="col-sm-2 img-inst">
+                    <div className="col-md-2 img-inst">
                         <img src={imgInstFive} alt="img"/>
                     </div>
-                    <div className="col-sm-2 img-inst">
+                    <div className="col-md-2 img-inst">
                         <img src={imgInstSix} alt="img"/>
                     </div>
                 </div>
